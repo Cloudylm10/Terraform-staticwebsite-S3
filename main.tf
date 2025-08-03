3ea27b4659bf477a030ca7fcd54159083501ee50
CloudyLM10 <lubhitdevops@gmail.com>
2025-08-04 01:33:44 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,77 @@
+#create s3 bucket
+resource "aws_s3_bucket" "mybucket" {
+  bucket = var.bucketname
+
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false   //false to make bucket public
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.mybucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.mybucket.id
+  key    = "index.html"
+  source = "index.html"
+  acl = "public-read"
+  content_type = "text/html"
+
+#   # The filemd5() function is available in Terraform 0.11.12 and later
+#   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+#   # etag = "${md5(file("path/to/file"))}"
+#   etag = filemd5("path/to/file")
+}
+
+resource "aws_s3_object" "error" {
+  bucket = aws_s3_bucket.mybucket.id
+  key    = "error.html"
+  source = "error.html"
+  acl = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "profile" {
+  bucket = aws_s3_bucket.mybucket.id
+  key = "profile.jpg"
+  source = "profile.jpg"
+  acl = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.mybucket.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "error.html"
+  }
+
+  depends_on = [ aws_s3_bucket_acl.example ]
+  
+}
